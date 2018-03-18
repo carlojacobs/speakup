0d67d172e98b05ad0db6f1edb59f007b2bc002cc
Carlo Jacobs <carlojacobs91@gmail.com>
2018-03-18 16:44:39 +0100

    Minor change

--- src/components/Share.js
+++ src/components/Share.js
@@ -5,11 +5,12 @@ import '../stylesheets/share.css';
 
 export default class Share extends Component {
   render() {
+    var name = this.props.match.params.name;
     return(
       <div className="container-fluid share">
-        <h1 id="shareH1">Thank you!</h1>
-        <h2 id="shareH2">You are now able to share this.</h2>
-        <p id="shareP">" I hereby declare that I will always respect my fellow man, regardless of religion, color, beliefs or way of life and I will never impose my own religion, beliefs or way of life on others, not with words nor violence in whatever way. Where others do, I will never remain silent but speak out loud. This is my contribution to a better world. "</p>
+        <h1 id="shareH1">Thank you, {name}!</h1>
+        <h2 id="shareH2">Share this message to spread the good thoughts.</h2>
+        <p id="shareP">"I, {name}, hereby declare that I will always respect my fellow man, regardless of religion, color, beliefs or way of life and I will never impose my own religion, beliefs or way of life on others, not with words nor violence in whatever way. Where others do, I will not remain silent but speak out loud. This is my contribution to a better world. "</p>
       </div>
     );
   }
