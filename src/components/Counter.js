f4659ef90e25ddf1efb83e253d755fea3c553ee8
Carlo Jacobs <carlojacobs91@gmail.com>
2018-03-18 16:38:14 +0100

    Minor change

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -15,7 +15,7 @@ export default class Counter extends Component {
   }
 
   getNum() {
-    axios.get('http://speakupforunity-api.com/num').then((res) => {
+    axios.get('https://speakupforunity-api.com/num').then((res) => {
       const number = res.data.num;
       this.setState({
         ...this.state,
